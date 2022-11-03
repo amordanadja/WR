41f9e7a84cbfff926af06d4de8c716d6c1fd84c5
gomeez <106769350+amordanadja@users.noreply.github.com>
2022-11-03 14:38:05 -0300

    Initial Commit

---
+++ WR/WRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WRApp.swift
+//  WR
+//
+//  Created by Ana Beatriz Costa Gomes on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
