5a546517cf37e34e15e47f8dfe008603e421ecd2
gomeez <106769350+amordanadja@users.noreply.github.com>
2022-11-03 15:24:08 -0300

    ola amigos

--- WR/ContentView.swift
+++ WR/ContentView.swift
@@ -11,9 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .font(.title)
+            Text("olá amigos!")
         }
         .padding()
     }
