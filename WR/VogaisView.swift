19c1748ec46f309fadcc2e2014955e7fee1121ec
YorranaOliveira <106768765+YorranaOliveira@users.noreply.github.com>
2022-11-10 17:43:20 -0300

    coloquei na tela vogal o texto parara

--- WR/VogaisView.swift
+++ WR/VogaisView.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct VogaisView: View {
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors:
-                [Color(red: 34/255, green: 66/255, blue: 128/255),
-                 Color(red: 132/255, green: 174/255, blue: 220/255),
-                 Color(red: 255/255, green: 255/255, blue: 255/255)]),
-                           
-                startPoint: .topLeading,
-                endPoint: .bottomLeading)
+        VStack {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors:
+                 [Color(red: 34/255, green: 66/255, blue: 128/255),
+                  Color(red: 132/255, green: 174/255, blue: 220/255),
+                  Color(red: 255/255, green: 255/255, blue: 255/255)]),
+                        
+                   startPoint: .topLeading,
+                   endPoint: .bottomLeading)
                 .ignoresSafeArea(.all, edges: .all)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color(red: 48/255, green: 63/255, blue: 129/255))
+                        .frame(width: 350, height: 70)
+                    Text("O ALFABETO É COMPOSTO POR \nVOGAIS, QUE SÃO:")
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                }
+            }
         }
     }
 }
