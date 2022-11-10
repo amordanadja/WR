511167204c02e6fcb6eab30a98cb3253f3d3873c
nadjaanasc <110944046+nadjaanasc@users.noreply.github.com>
2022-11-10 14:24:43 -0300

    Criação dos arquivos (EscolhaDoUsuarioView e VogaisView)

---
+++ WR/EscolhaDoUsuarioView.swift
@@ -0,0 +1,22 @@
+//
+//  EscolhaDoUsuarioView.swift
+//  WR
+//
+//  Created by Nadja Maria Nascimento de Souza on 10/11/22.
+//
+
+import SwiftUI
+
+struct EscolhaDoUsuarioView: View {
+    var body: some View {
+        VStack {
+            Text ("ALFABETO")
+        }
+    }
+}
+
+struct EscolhaDoUsuarioView_Previews: PreviewProvider {
+    static var previews: some View {
+        EscolhaDoUsuarioView()
+    }
+}
---
+++ WR/VogaisView.swift
@@ -0,0 +1,22 @@
+//
+//  VogaisView.swift
+//  WR
+//
+//  Created by Nadja Maria Nascimento de Souza on 10/11/22.
+//
+
+import SwiftUI
+
+struct VogaisView: View {
+    var body: some View {
+        VStack{
+           
+        }
+    }
+}
+
+struct VogaisView_Previews: PreviewProvider {
+    static var previews: some View {
+        VogaisView()
+    }
+}
