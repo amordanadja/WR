dc14dbd0361376b968b6502c83e35cd97ef0cc82
gomeez <106769350+amordanadja@users.noreply.github.com>
2022-12-01 14:57:18 -0300

    ajeitamos a tela das vogais

--- WR/VogaisView.swift
+++ WR/VogaisView.swift
@@ -8,107 +8,103 @@
 import SwiftUI
 
 struct VogaisView: View {
-    var body: some View {
-        VStack() {
-            ZStack {
-                LinearGradient(gradient: Gradient(colors:
+    
+    var vogais: [String] = ["A", "E", "I", "O", "U"]
+    
+    fileprivate func backgroundGradient() -> some View {
+        return LinearGradient(gradient: Gradient(colors:
                                                     [Color(red: 34/255, green: 66/255, blue: 128/255),
                                                      Color(red: 132/255, green: 174/255, blue: 220/255),
                                                      Color(red: 255/255, green: 255/255, blue: 255/255)]),
-                               
-                               startPoint: .topLeading,
-                               endPoint: .bottomLeading)
-                .ignoresSafeArea(.all, edges: .all)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 25)
-                        .fill(Color(red: 48/255, green: 63/255, blue: 129/255))
-                        .frame(width: 350, height: 70)
-                    Text("O ALFABETO É COMPOSTO POR \nVOGAIS, QUE SÃO:")
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                }
-            }
-            VStack(spacing:30) {
-                HStack{
-                    Button {
-                        print("Imagem tapped!")
-                    } label: {
-                        Image("A")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
+                              
+                              startPoint: .topLeading,
+                              endPoint: .bottomLeading)
+        .ignoresSafeArea(.all, edges: .all)
+    }
+    
+    fileprivate func textBox() -> some View {
+        return ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color(red: 48/255, green: 63/255, blue: 129/255))
+                .frame(width: 350, height: 70)
+            Text("O ALFABETO É COMPOSTO POR \nVOGAIS, QUE SÃO:")
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+        }
+        .padding(30)
+    }
+    
+    fileprivate func gridvogais() -> some View {
+        return LazyVGrid(
+            columns: Array(repeating:GridItem(spacing: 30), count: 3),
+            alignment: .center,
+            spacing: 30
+        ) {
+            
+            ForEach(vogais, id: \.self) { vogal in
+                Button {
+                    print (vogal)
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color(red: 48/255, green: 63/255, blue: 129/255))
+                            .aspectRatio(1, contentMode: .fit)
+                            .cornerRadius(30)
+                         
+                            
+                        
+                        Text("\(vogal)")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .bold()
+                        
                         
                     }
-                    
-                    .padding(10)
-                    
-                    Button {
-                        print("Image tapped!")
-                    } label: {
-                        Image("E")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                    }
-                    
-                    .padding(10)
-                    Button {
-                        print("Image tapped!")
-                    } label: {
-                        Image("I")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                    }
-                    .padding(10)
-                }
-                
-                
-                HStack{
-                    Button {
-                        print("Image tapped!")
-                    } label: {
-                        Image("O")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                    }
-                    
-                    .padding()
-                    
-                    Button {
-                        print("Image tapped!")
-                    } label: {
-                        Image("U")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                    }
-                    
                 }
             }
         }
+        .padding(30)
     }
-}
-
-struct vogais: View {
-    var body: some View{
-        VStack{
-         TabView{
-          ContentView()
-          EscolhaDoUsuarioView()
-          VogaisView()
-          AtividadeVogaisView()
-          ConsoantesUIView()
+    
+    var body: some View {
+        VStack() {
+            ZStack {
+                backgroundGradient()
+                
+                VStack(alignment: .center, spacing: 90) {
+                    textBox()
+                    gridvogais()
+                }
             }
-         .tabViewStyle(.page(indexDisplayMode: .always))
-        }.background().ignoresSafeArea()
+            
+        }
+        
+        
     }
-}
-
-struct VogaisView_Previews: PreviewProvider {
-    static var previews: some View {
-vogais()
-  }
+    
+    
+    
+    
+    
+    
+    struct Vogais: View {
+        var body: some View{
+            VStack{
+                TabView{
+                    ContentView()
+                    EscolhaDoUsuarioView()
+                    VogaisView()
+                    AtividadeVogaisView()
+                    ConsoantesUIView()
+                }
+                .tabViewStyle(.page(indexDisplayMode: .always))
+            }.background().ignoresSafeArea()
+        }
+    }
+    
+    struct VogaisView_Previews: PreviewProvider {
+        static var previews: some View {
+            Vogais()
+        }
+    }
 }
